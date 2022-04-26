dd7471da8e2a22066d4456dce4c6bf6ecef555f1
Austin Flynn <flynna237@gmail.com>
2022-04-26 18:50:19 -0500

    Added python functions
    Added functions to the main python file for each of the queries and opening and closing connection

--- project.py
+++ project.py
@@ -1,58 +1,79 @@
 from mysql.connector import *
 
-try:
-    # Set connection to the database
-    with connect(
+def establish_con():
+    global connection 
+    connection = connect(
         # Enter mySQL credentials
         user='abflynn',
         password='Pex9oof6',
-        host='125.1.1.1',
-    ) as connection:
-        print(connection)
+        host='localhost',
+        database='abflynn'
+    )
 
-        # Queries for each user option
-        add_investor = ('INSERT INTO Investors '
-                        '(InvestorId, Name, Email) '
-                        'VALUES (%s, %s, %s)')
+    global cursor 
+    cursor = connection.cursor()
 
-        add_cryptocurrency = ('INSERT INTO Cryptocurrency '
-                            '(CryptocurrencyId, CryptoName, CurrentValue) '
-                            'VALUES (%s, %s, %s)')
+def close_con():
+    cursor.close()
+    connection.close()
 
-        buy_investment  = ('INSERT INTO Investment '
-                            '(InvestorId, CryptocurrencyId, NumShares, PurchasePrice, StillOwned) '
-                            'VALUES (%s, %s, %s, %s, %s)')
+def new_investor(id, name, email):
+    add_investor = ('INSERT INTO Investors '
+                    '(InvestorId, Name, Email) '
+                    'VALUES (%s, %s, %s)'
+                    (id, name, email))
+    cursor.execute(add_investor)
 
-        sell_investment = ('UPDATE Investment '
-                            'SET (NumShares, PurchasePrice, StillOwned) '
-                            'VALUES (%s, %s, %s)')
+def new_crypto(id, name, value):
+    add_cryptocurrency = ('INSERT INTO Cryptocurrency '
+                        '(CryptocurrencyId, CryptoName, CurrentValue) '
+                        'VALUES (%s, %s, %s)'
+                        (id, name, value))
+    cursor.execute(add_cryptocurrency)
 
-        view_investment = ('SELECT (CurrentValue * NumShares) AS TotalValue, CryptoName '
-                            'FROM  ('
-                                'SELECT InvestorId, CurrentValue, NumShares, CryptoName FROM '
-                                'Investment INNER JOIN '
-                                'Cryptocurrency ON '
-                                'Investment.CryptocurrencyId = Cryptocurrency.CryptocurrencyId '
-                                'ORDER BY PurchasePrice '
-                            ') AS A '
-                            'WHERE A.InvestorId = %s '
-                            'ORDER BY TotalValue DESC')
+def buy(investor, crypto, shares, price, owned):
+    buy_investment  = ('INSERT INTO Investment '
+                        '(InvestorId, CryptocurrencyId, NumShares, PurchasePrice, StillOwned) '
+                        'VALUES (%s, %s, %s, %s, %s)'
+                        (investor, crypto, shares, price, owned))
+    cursor.execute(buy_investment)
 
-        view_investors = ('SELECT Name, SUM(CurrentValue) as TotalShares '
-                            'FROM  ('
-                                'SELECT InvestorId, CurrentValue, NumShares, Cryptocurrency.CryptocurrencyId FROM'
-                                'Investment INNER JOIN'
-                                'Cryptocurrency ON'
-                                'Investment.CryptocurrencyId = Cryptocurrency.CryptocurrencyId'
-                                'ORDER BY PurchasePrice'
-                            ') AS A'
-                            'LEFT JOIN Investor'
-                            'on A.InvestorId = Investor.InvestorId'
-                            'WHERE CryptocurrencyId = %s'
-                            'GROUP BY Name')
+def sell(shares, price, owned):
+    sell_investment = ('UPDATE Investment '
+                    'SET (NumShares, PurchasePrice, StillOwned) '
+                    'VALUES (%s, %s, %s)'
+                    (shares, price, owned))
+    cursor.execute(sell_investment)
 
-        
+def view_investments(id):
+    view_investment = ('SELECT (CurrentValue * NumShares) AS TotalValue, CryptoName '
+                    'FROM  ('
+                        'SELECT InvestorId, CurrentValue, NumShares, CryptoName FROM '
+                        'Investment INNER JOIN '
+                        'Cryptocurrency ON '
+                        'Investment.CryptocurrencyId = Cryptocurrency.CryptocurrencyId '
+                        'ORDER BY PurchasePrice '
+                    ') AS A '
+                    'WHERE A.InvestorId = %s '
+                    'ORDER BY TotalValue DESC'
+                    (id))
+    cursor.execute(view_investment)
 
-# Display connection error if there is one
-except Error as e:
-    print(e)
+def view_investors(id):
+    view_investors = ('SELECT Name, SUM(CurrentValue) as TotalShares '
+                    'FROM  ('
+                        'SELECT InvestorId, CurrentValue, NumShares, Cryptocurrency.CryptocurrencyId FROM'
+                        'Investment INNER JOIN'
+                        'Cryptocurrency ON'
+                        'Investment.CryptocurrencyId = Cryptocurrency.CryptocurrencyId'
+                        'ORDER BY PurchasePrice'
+                    ') AS A'
+                    'LEFT JOIN Investor'
+                    'on A.InvestorId = Investor.InvestorId'
+                    'WHERE CryptocurrencyId = %s'
+                    'GROUP BY Name'
+                    (id))
+    cursor.execute(view_investors)
+
+def main():
+    pass
